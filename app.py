4c123d7d8bbe6fe6b8b5093f1015db2e1bb17333
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-10-27 00:56:58 +0000

    creat sql queries to fetch local and remote requests, send objects to index.html

--- app.py
+++ app.py
@@ -16,19 +16,31 @@ def index():
     # Get number of all GET requests
     sql_all = """SELECT COUNT(*) FROM weblogs;"""
     cur.execute(sql_all)
-    all = cur.fetchone()[0]
+    all_req = cur.fetchone()[0]
 
     # Get number of all succesful requests
     sql_success = """SELECT COUNT(*) FROM weblogs WHERE status LIKE \'2__\';"""
     cur.execute(sql_success)
     success = cur.fetchone()[0]
 
+    # Get number of all remote requests
+    sql_remote = """SELECT COUNT(*) FROM weblogs WHERE source = \'remote\' AND status LIKE \'2__\';"""
+    cur.execute(sql_remote)
+    remote = cur.fetchone()[0]
+
+    # get number of all local requests
+    sql_local = """SELECT COUNT(*) FROM weblogs WHERE source = \'local\' AND status LIKE \'2__\';"""
+    cur.execute(sql_local)
+    local = cur.fetchone()[0]
+
     # Determine rate if there was at least one request
     rate = "No entries yet!"
-    if all != 0:
-        rate = str(success / all)
+    if all_req != 0:
+        rate = str(success / all_req)
+        local_rate = str(local / all_req)
+        remote_rate = str(remote / all_req)
 
-    return render_template('index.html', rate = rate)
+    return render_template('index.html', rate=rate, local_rate=local_rate, remote_rate=remote_rate)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
